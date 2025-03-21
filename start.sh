240bda0558a99f2ce2e6e3634c06cc2a3ee1fe8c
tua-qwest <33480444+tua-qwest@users.noreply.github.com>
2025-03-21 23:11:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ if [ ! -f $VIRTUALENV/bin/pip ]; then
 fi
 
 # Install the requirements
-$VIRTUALENV/bin/pip install -r requirements.txt
+$VIRTUALENV/bin/pip3 install -r requirements.txt
 
 # Run your glorious application
 $VIRTUALENV/bin/python3 main.py
